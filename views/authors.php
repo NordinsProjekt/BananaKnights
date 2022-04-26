d4bf5008e1558e22e1b1259c070e23e2a4f1cca4
Markus Nordin <82381561+NordinsProjekt@users.noreply.github.com>
2022-04-26 22:23:53 +0200

    Author
    Lagt till bilder för Author

--- views/authors.php
+++ views/authors.php
@@ -39,8 +39,10 @@ function ShowAllAuthors($arr,$role)
 
 function ShowAuthor($dataArr,$role)
 {
+    var_dump($dataArr);
     $text = "<h1>Visa Författare</h1>";
     $text .= "<h2>".$dataArr['Author']['Firstname']." " . $dataArr['Author']['Lastname'] ."</h2>";
+    $text .= "<img src='".$dataArr['Author']['ImageLink']."' />";
     $text .= "<p><b>Land:</b> ".$dataArr['Author']['Country']."<br />";
     $text .= "<b>Född:</b> " .$dataArr['Author']['Born']."<br />";
 
@@ -76,13 +78,14 @@ function ShowAuthor($dataArr,$role)
 function AddNewAuthor()
 {
     $text = "<h1>Skapa ny författare</h1>";
-    $text .= "<form method='post' action='".prefix."author/addauthor'>";
+    $text .= "<form method='post' action='".prefix."author/addauthor' enctype='multipart/form-data'>";
     $text .= "<table>";
     $text .= "<tr><td><label for='firstname'>Firstname</label></td> <td><input type='text' class='form-control' id='firstname' name='Fname' required /></td> </tr>";  
     $text .= "<tr><td><label for='lastname'>Lastname</label></td> <td><input type='text' class='form-control' id='lastname' name='Lname' required /></td> </tr>"; 
     $text .= "<tr><td><label for='country'>Country</label></td> <td><input type='text' class='form-control' id='country' name='Country' required /></td> </tr>"; 
     $text .= "<tr><td><label for='born'>Born</label></td> <td><input type='date' class='form-control' id='born' name='Born' required/></td> </tr>"; 
-    $text .= "<tr><td><label for='death'>Death</label></td> <td><input type='date' class='form-control' id='death' name='Death' /></td> </tr>"; 
+    $text .= "<tr><td><label for='death'>Death</label></td> <td><input type='date' class='form-control' id='death' name='Death' /></td> </tr>";
+    $text .= "<tr> <td><label for='txtAuthorPicture'>Bild</label></td> <td><input type='file' class='form-control' id='txtAuthorPicture' name='AuthorPicture' /></td> </tr>";
 
     // behöver lägga till bild table i db
     //$text .= "<tr> <td><label for='authorpic'>Bild</label></td> <td><input type='file' id='authorpic' name='AuthorPic' /></td> </tr>";
